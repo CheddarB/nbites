c292b7f756344326e4cd784d1c28aee30b0ef054
Evan <evanchoyt@gmail.com>
2015-07-16 22:13:40 -0400

    log only images with corners

--- src/man/vision/VisionModule.cpp
+++ src/man/vision/VisionModule.cpp
@@ -283,7 +283,7 @@ void VisionModule::run_()
 
         PROF_EXIT2(P_VISION_TOP, P_VISION_BOT, i==0)
 #ifdef USE_LOGGING
-        if (!i && !centerCircleDetector[i]->on())
+        if (cornerDetector[i]->size() > 0)
             logImage(i);
 #endif
     }
